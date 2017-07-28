0e7e7c64934ef9d6ad759745fe600b6b700f8249
Zerglrisk <wmf1235@naver.com>
2017-07-29 00:29:08 +0900

    Create Project and v0.0.1
    v0.0.1

---
+++ GetFilePermissions/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace GetFilePermissions
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label1.Location = new System.Drawing.Point(0, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(250, 15);
+            this.label1.TabIndex = 1;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button2.Location = new System.Drawing.Point(175, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 105);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "모든 권한 \r\n\r\n제거";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 105);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "모든 권한 \r\n\r\n획득";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button3.Location = new System.Drawing.Point(75, 0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 105);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "제작자\r\n\r\n정보";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(250, 120);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Win 10 권한 획득 프로그램";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ GetFilePermissions/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace GetFilePermissions
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //this.Size = new Size(Screen.PrimaryScreen.Bounds.Width / 10, Screen.PrimaryScreen.Bounds.Height / 10);
+        }
+
+        //All Getting Authority
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RegistryKey rkey = Registry.ClassesRoot.CreateSubKey("*\\shell\\runas");
+            rkey.SetValue("", "모든 권한 얻기", RegistryValueKind.String);
+            rkey.SetValue("NoWorkingDirectory", "", RegistryValueKind.String);
+            rkey = Registry.ClassesRoot.CreateSubKey("*\\shell\\runas\\command");
+            rkey.SetValue("", "cmd.exe /c takeown /f \"%1\" && icacls \"%1\" /grant administrators:F", RegistryValueKind.String);
+            rkey.SetValue("IsolatedCommand", "cmd.exe /c takeown /f \"%1\" && icacls \"%1\" /grant administrators:F", RegistryValueKind.String);
+            rkey = Registry.ClassesRoot.CreateSubKey("exefile\\shell\\runas2");
+            rkey.SetValue("", "모든 권한 얻기", RegistryValueKind.String);
+            rkey.SetValue("NoWorkingDirectory", "", RegistryValueKind.String);
+            rkey = Registry.ClassesRoot.CreateSubKey("exefile\\shell\\runas2\\command");
+            rkey.SetValue("", "cmd.exe /c takeown /f \"%1\" && icacls \"%1\" /grant administrators:F", RegistryValueKind.String);
+            rkey.SetValue("IsolatedCommand", "cmd.exe /c takeown /f \"%1\" && icacls \"%1\" /grant administrators:F", RegistryValueKind.String);
+            rkey = Registry.ClassesRoot.CreateSubKey("Directory\\shell\\runas");
+            rkey.SetValue("", "모든 권한 얻기", RegistryValueKind.String);
+            rkey.SetValue("NoWorkingDirectory", "", RegistryValueKind.String);
+            rkey = Registry.ClassesRoot.CreateSubKey("Directory\\shell\\runas\\command");
+            rkey.SetValue("", "cmd.exe /c takeown /f \"%1\" /r /d y && icacls \"%1\" /grant administrators:F /t", RegistryValueKind.String);
+            rkey.SetValue("IsolatedCommand", "cmd.exe /c takeown /f \"%1\" /r /d y && icacls \"%1\" /grant administrators:F /t", RegistryValueKind.String);
+            rkey.Close();
+        }
+
+        //All Deleting Authority
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Registry.ClassesRoot.DeleteSubKey("*\\shell\\runas\\command", false);
+                Registry.ClassesRoot.DeleteSubKey("*\\shell\\runas", false);
+                Registry.ClassesRoot.DeleteSubKey("exefile\\shell\\runas2\\command", false);
+                Registry.ClassesRoot.DeleteSubKey("exefile\\shell\\runas2", false);
+                Registry.ClassesRoot.DeleteSubKey("Directory\\shell\\runas\\command", false);
+                Registry.ClassesRoot.DeleteSubKey("Directory\\shell\\runas", false);
+            }
+
+            catch (System.InvalidOperationException err)
+            {
+                if(MessageBox.Show("권한 제거에 실패하였습니다.\n강제로 삭제하시겠습니까?\n이 행동으로 인한 결과의 책임은 지지않습니다.","",MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    Registry.ClassesRoot.DeleteSubKeyTree("*\\shell\\runas", false);
+                    Registry.ClassesRoot.DeleteSubKeyTree("exefile\\shell\\runas2", false);
+                    Registry.ClassesRoot.DeleteSubKeyTree("Directory\\shell\\runas", false);
+                }
+            }
+        }
+
+        //about
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Github : https://github.com/Zerglrisk/GetFilePermissions\n"
+                + "Origin Resource : http://jungle-e.tistory.com/385 \n"
+                + "Made by : Zerglrisk"
+                + "Version : 0.0.1");
+        }
+    }
+}
